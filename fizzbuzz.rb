f6796cc550a57c8769bf5053e49ccee770830cf2
Aleysha Ukonu <aleysha2000@outlook.com>
2020-06-15 00:08:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (int)
+  if int % 3 == 0 && int % 5 == 0 
+    puts "FizzBuzz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  end
+end
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
